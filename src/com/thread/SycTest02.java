5291eb18acd74b1415f31964b356d37db94fc8d2
WendaZheng <dylan.d.zheng@gmail.com>
2021-04-14 13:57:36 +0200

    Syc test

--- src/com/thread/SycTest02.java
+++ src/com/thread/SycTest02.java
@@ -1,4 +1,17 @@
 package com.thread;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SycTest02 {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        for(int i=0;i<10000;i++){
+            new Thread(()->{
+                list.add(Thread.currentThread().getName());
+            }).start();
+        }
+        System.out.println(list.size());
+    }
 }
